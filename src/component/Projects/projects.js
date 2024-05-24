626e0a029abb72cf2c1fbcc0cab50bbb64e3254b
Isaac Hall <106003051+Isaac-Hall1@users.noreply.github.com>
2024-05-23 23:20:36 -0600

    Merge to Main

--- src/component/Projects/projects.js
+++ src/component/Projects/projects.js
@@ -4,17 +4,33 @@ import './projects.css';
 const Projects = () => {
   return(
     <div id = "projects" className = "projects">
-      <button className = "projectOne">
-
+      <button className="button">
+        <img src="path/to/project1.jpg" alt="Project 1" />
+        <div className="button-text">
+          <h3>Project 1</h3>
+          <p>Summary of project 1.</p>
+        </div>
       </button>
-      <button className = "projectTwo">
-        
+      <button className="button">
+        <img src="path/to/project2.jpg" alt="Project 2" />
+        <div className="button-text">
+          <h3>Project 2</h3>
+          <p>Summary of project 2.</p>
+        </div>
       </button>
-      <button className = "projectThree">
-
+      <button className="button">
+        <img src="path/to/project3.jpg" alt="Project 3" />
+        <div className="button-text">
+          <h3>Project 3</h3>
+          <p>Summary of project 3.</p>
+        </div>
       </button>
-      <button className = "projectFour">
-        
+      <button className="button">
+        <img src="path/to/project4.jpg" alt="Project 4" />
+        <div className="button-text">
+          <h3>Project 4</h3>
+          <p>Summary of project 4.</p>
+        </div>
       </button>
     </div>
   )
